8283b5446cecedad0b2883e0ec20a262c112a2fb
Imanpal Singh <imanpal@kiprosh.com>
2022-05-14 21:53:37 +0530

    refactor driver logic

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,57 +1,44 @@
-import { createInterface } from 'readline';
-import { join } from 'path';
 import { createWriteStream } from 'fs';
+import { join } from 'path'; import { createInterface } from 'readline';
+import { ConfigurationType } from '../../types/domain.js';
+import { Parser } from '../parser/parse.js';
 import { createPgDump } from '../pg/dump.js';
-import { info, success } from '../utils/loggers.js';
-import { ConfigurationType } from '../../types/configuration.js';
-import { Parser } from './parse.js';
-import { PgExtractors } from '../pg/extracters.js';
-import { gracefulShutdown } from '../utils/handlers.js';
+import { PgExtractor } from '../pg/extractors.js';
 
 export async function cli(config: string, args: { [arg: string]: string }) {
   const importPath = join(process.cwd(), config);
   const { configuration }: { configuration: ConfigurationType } = await import(importPath);
 
-  const parser = new Parser(configuration);
-  parser.verifyConfiguration();
-  const pg = createPgDump(parser.connectionUrl);
+  const pgDump = createPgDump(configuration.connectionUrl);
 
-  info(`Starting dump > ${args.output}`);
-
-  const inputLineResults = createInterface({
-    input: pg.stdout,
+  const inputStream = createInterface({
+    input: pgDump.stdout,
     crlfDelay: Infinity,
   }) as any as Iterable<string>;
-  const out = createWriteStream(args.output);
 
-  let table: string | null = null;
-  let cols: string[] = [];
+  const outputStream = createWriteStream(args.output);
+  let canWriteToFile = true;
+  const parser = new Parser(configuration);
 
-  for await (let line of inputLineResults) {
-    const data = new PgExtractors(line);
+  for await (let line of inputStream) {
+    if (PgExtractor.isAComment(line)) {
+      canWriteToFile = false;
+    } else {
+      const query = PgExtractor.parseLine(line);
 
-    try {
-      if (data.isCopyQuery()) {
-        table = data.getTableName();
-        cols = data.getCols();
-        /*
-          Actual data is on the next line of pg_dump
-        */
-      } else if (table && cols.length && line.trim() && (line !== '\\.')) {
-        line = line
-          .split('\t')
-          // eslint-disable-next-line no-loop-func
-          .map((value, index) => parser.applyTransform(value, table as string, cols[index]))
-          .join('\t');
-      } else {
-        table = null;
-        cols = [];
+      if (query) {
+        parser.reset();
+        canWriteToFile = true;
+        if (parser.shouldExecute(query)) {
+          parser.parseQuery(query);
+        } else {
+          canWriteToFile = false;
+        }
+      } else if (parser.flags.queryDataInProgress) {
+        line = parser.parseData(line);
       }
-
-      out.write(`${line}\n`);
-    } catch (e) {
-      gracefulShutdown(e as string);
     }
+
+    if (canWriteToFile) outputStream.write(`${line}\n`);
   }
-  success(`Dump Completed : ${args.output}`);
 }
