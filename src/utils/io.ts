dbb3e820bc70613d963c1afede5316f484b12edd
Imanpal Singh <imanpal@kiprosh.com>
2022-05-21 16:43:00 +0530

    allow output to stdout

---
+++ src/utils/io.ts
@@ -0,0 +1,17 @@
+import {createWriteStream} from 'fs';
+import {stdout} from 'process';
+import {createInterface} from 'readline';
+
+export function createInputStream(stream: any) {
+  return createInterface({
+    input: stream,
+    crlfDelay: Infinity,
+  }) as any as Iterable<string>;
+}
+
+export function createOutputStream(stream: string) {
+  if (stream === 'STDOUT') {
+    return stdout;
+  }
+  return createWriteStream(stream);
+}
