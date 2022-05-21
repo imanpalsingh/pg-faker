94d872daa29ff84884bae5831b5ebb14fd1f8f1c
Imanpal Singh <imanpal@kiprosh.com>
2022-05-21 21:36:35 +0530

    add new option for verbosity

--- src/cli/lex.ts
+++ src/cli/lex.ts
@@ -4,6 +4,6 @@ import {cli} from './cli.js';
 const command = new Command();
 
 // eslint-disable-next-line max-len
-command.argument('<config>').description('Path to the js configuration file').option('-o --output <outFile>', 'Name of the output sql file', 'dump.sql').action(cli);
+command.argument('<config>').description('Path to the js configuration file'). option('-o --output <outFile>', 'Name of the output sql file', 'dump.sql').option('-v --verbose <mode>', 'Verbose mode', 'verbose').action(cli);
 
 export {command};
