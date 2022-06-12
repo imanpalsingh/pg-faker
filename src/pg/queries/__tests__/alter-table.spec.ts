a8eced8adc66e63f6a8e7908bb78d3e92948d094
Imanpal Singh <imanpal@kiprosh.com>
2022-06-12 11:57:42 +0530

    Fix Alter Table query
    Alter table query was not handling a variation of ONLY query
    This commit also includes the updated test cases for the same

--- src/pg/queries/__tests__/alter-table.spec.ts
+++ src/pg/queries/__tests__/alter-table.spec.ts
@@ -1,18 +1,25 @@
 import {AlterTable} from '../alter-table';
 
-
 describe('Alter table', () => {
   const table = 'users';
   const query = `ALTER TABLE public.${table} OWNER TO PGFAKER;`;
+  const onlyQuery = `ALTER TABLE ONLY public.${table}`;
 
   it('Identifies correct queries', () => {
     const matcher = new AlterTable();
     expect(matcher.match(query)).not.toHaveLength(0);
+    expect(matcher.match(onlyQuery)).not.toHaveLength(0);
   });
 
-  it('It has correct table name', () => {
+  it('It has correct table name for normal query', () => {
     const matcher = new AlterTable();
     matcher.query = query;
+    expect(matcher.tableName).toBe(table);
+  });
+
+  it('It has correct table name for ONLY query', () => {
+    const matcher = new AlterTable();
+    matcher.query = onlyQuery;
     expect(matcher.tableName).toBe(table);
   });
 });
