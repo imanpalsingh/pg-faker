010dbefe13de53ae00e51f4f25b970362106f266
Imanpal Singh <imanpal@kiprosh.com>
2022-07-03 11:23:59 +0530

    fix formatting

--- src/pg/queries/abstracts/data-query.ts
+++ src/pg/queries/abstracts/data-query.ts
@@ -6,17 +6,17 @@ class DataQuery extends Query {
   constructor() {
     super();
     if (this.constructor === DataQuery) {
-      throw new Error('Abstract class DataQuery need can\'t be instantiated.');
+      throw new Error(`Abstract class DataQuery need can\'t be instantiated.`);
     }
   }
 
   get columns() {
     return this.query
-        .replace(this.columnRegex, '$1')
-        .split(',')
-        .map((e) => e.trim())
-        .map((e) => e.replace(/"/g, ''))
-        .map((e) => e.toLowerCase());
+      .replace(this.columnRegex, '$1')
+      .split(',')
+      .map((e) => e.trim())
+      .map((e) => e.replace(/"/g, ''))
+      .map((e) => e.toLowerCase());
   }
 }
 
