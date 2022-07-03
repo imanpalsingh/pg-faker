d56aef37b2b66a0e47239c5bc5d1fe29c868e102
Imanpal Singh <imanpal@kiprosh.com>
2022-07-03 11:23:51 +0530

    add new query registrar

---
+++ src/pg/queries/index.ts
@@ -0,0 +1,28 @@
+/*
+    Register the queries here.
+    If the regex are not specific enough, then the order of the queries will matter
+*/
+
+import alterSequence from './alter-sequence.js';
+import alterTable from './alter-table.js';
+import alterTableSequence from './alter-table-sequence.js';
+import copy from './copy.js';
+import createSequence from './create-sequence.js';
+import createTable from './create-table.js';
+import sequenceSet from './sequence-set.js';
+import setQ from './set-q.js';
+import select from './select.js';
+import createIndex from './create-index.js';
+
+export const queries = [
+  alterSequence,
+  alterTableSequence,
+  alterTable,
+  copy,
+  createSequence,
+  createTable,
+  sequenceSet,
+  setQ,
+  select,
+  createIndex,
+];
