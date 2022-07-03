a521881fd6ca0d947813b1cad8e54970fc7156bb
Imanpal Singh <imanpal@kiprosh.com>
2022-07-03 11:22:51 +0530

    attach new driver to the command

--- src/cli/lex.ts
+++ src/cli/lex.ts
@@ -1,9 +1,14 @@
 import {Command} from 'commander';
-import {cli} from './cli.js';
+import driver from './driver';
 
 const command = new Command();
 
 // eslint-disable-next-line max-len
-command.argument('<config>').description('Path to the js configuration file'). option('-o --output <outFile>', 'Name of the output sql file', 'dump.sql').option('-v --verbose <mode>', 'Verbose mode', 'verbose').action(cli);
+command
+  .argument('<config>')
+  .description('Path to the js configuration file')
+  .option('-o --output <outFile>', 'Name of the output sql file', 'dump.sql')
+  .option('-v --verbose <mode>', 'Verbose mode', 'verbose')
+  .action(driver.run);
 
 export {command};
